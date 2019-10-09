87994fb2a88ed16f61e4311a849899af2a2bd8ef
BARON <anthony.baron@capgemini.com>
2019-10-09 06:34:49 +0200

    init project

---
+++ src/main/java/fr/epsi/test/cinema/Thanos.java
@@ -0,0 +1,56 @@
+package fr.epsi.test.cinema;
+
+public class Thanos {
+
+	int nbPierreInfinite;
+	boolean missionReussi;
+
+	public Thanos(int nbPierreInfinite) {
+		if(nbPierreInfinite <=5) {
+			this.nbPierreInfinite = nbPierreInfinite;
+		}else {
+			this.nbPierreInfinite = 5;
+		}
+		this.missionReussi = false;
+	}
+
+	public void gagnePierre() {
+		if (this.nbPierreInfinite < 5) {
+			this.nbPierreInfinite++;
+		}
+	}
+
+	public int claquementDeDoigts(int nbPopulation) {
+		int result;
+		if (this.nbPierreInfinite == 5) {
+			result = nbPopulation / 2;
+			this.missionReussi = true;
+		} else {
+			result = nbPopulation;
+		}
+		return result;
+	}
+
+	public int getNbPierreInfinite() {
+		return nbPierreInfinite;
+	}
+
+	public void setNbPierreInfinite(int nbPierreInfinite) {
+		this.nbPierreInfinite = nbPierreInfinite;
+	}
+
+	public boolean isMissionReussi() {
+		return missionReussi;
+	}
+
+	public void setMissionReussi(boolean missionReussi) {
+		this.missionReussi = missionReussi;
+	}
+
+	@Override
+	public String toString() {
+		return "Thanos [nbPierreInfinite=" + nbPierreInfinite + ", missionReussi=" + missionReussi + "]";
+	}
+	
+
+}
